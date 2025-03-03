035ac169ee0602a05edb82389bffc8e3e2062fbe
Sahar <ghrairisahar@gmail.com>
2025-03-03 16:57:58 +0100

    commit

---
+++ saharghrairi-js-001.js
@@ -0,0 +1,15 @@
+let num1 = 1;
+console.log(num1);
+
+let num2 = 2;
+console.log(num2);
+
+let stringa = "Ciao mi chiamo Sahar";
+console.log(stringa);
+stringa = 5.5;
+console.log(stringa);
+
+const PI_GRECO = 3.14;
+console.log(PI_GRECO);
+PI_GRECO = 5;
+console.log(PI_GRECO);
